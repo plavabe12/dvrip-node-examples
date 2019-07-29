b27cb590d0dbdb64f5f6082a8563cd96536bed89
plavabe12 <23537510+plavabe12@users.noreply.github.com>
2019-07-28 17:59:10 -0700

    Finding Camera and pushing to a list

--- index.js
+++ index.js
@@ -3,39 +3,23 @@ var
   http = require('http'),
   Cam = require('onvif').Cam;
 
-var PORT_TO_LISTEN = 3030;
-
+var ipList = [];
 onvif.Discovery.on('device', function(cam,rinfo,xml){
-    // function will be called as soon as NVT responses
-    console.log('Reply from ' + rinfo.address);
-    console.log(cam.hostname + ':' + cam.port + cam.path);
-    cam.username = 'admin';
-    cam.password = 'tlJwpbo6';
-
-    var CAMERA_HOST = cam.hostname,
-    USERNAME = cam.username,
-    PASSWORD = cam.password ,
-    PORT = cam.port;
-
-    var test_cam = new Cam({
-      hostname: CAMERA_HOST,
-      username: USERNAME,
-      password: PASSWORD,
-      port: PORT
+    var rtsp_cam = new Cam({
+      hostname: cam.hostname,
+      username: 'admin',
+      password: 'tlJwpbo6',
+      port: cam.port
     }, function(err) {
-      if (err) {
-        console.log('Connection Failed for ' + CAMERA_HOST + ' Port: ' + PORT + ' Username: ' + USERNAME + ' Password: ' + PASSWORD);
-        return;
-      }
-      console.log('CONNECTED');
-      this.absoluteMove({
-        x: 1
-        , y: 1
-        , zoom: 1
+      this.getStreamUri({protocol:'RTSP'}, function(err, stream) {
+        ipList.push({'ip' : rtsp_cam.hostname,'uri':  stream.uri});
       });
-      this.getStreamUri({protocol:'RTSP'}, function(err, stream) {console.log(stream.uri);});
-      console.log(this.profiles);
       });
-
 })
-onvif.Discovery.probe();
+
+onvif.Discovery.probe(timeout=1000);
+
+
+setTimeout(function(){
+  console.log(ipList);
+}, 1200);
